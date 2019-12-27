0ddd1a5e2bf126c276aa4a112916ca549cf73d20
Hardy Clervil <clervilh@gmail.com>
2019-12-27 15:29:33 -0500

    Update for blog project

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,22 @@
 class ArticlesController < ApplicationController
 	def new
+		@article = Article.new
 	end
 
 	def create
-		@article = Article.new(params.require(:article).permit(:title, :text))
+		@article = Article.new(article_params)
 
-		@article.save
-		
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 
 	def show
 		@article = Article.find(params[:id])
@@ -17,4 +25,10 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
 end
